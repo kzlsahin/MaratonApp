7b94a389d1ddb125badf351883b064c40a7ce941
kzlsahin <senturkmus@gmail.com>
2022-07-05 22:31:20 +0300

    Comment added

--- ExamApp1/Program.cs
+++ ExamApp1/Program.cs
@@ -14,7 +14,7 @@ namespace MaratonApp
     {
         Console.WriteLine("Bu uygulama girdiğiniz koşu süreleri ve adım mesafenize göre koştuğunuz mesafeyi hesaplar");
         Console.WriteLine("Maraton koşunuz farklı aşamalardan oluşuyorsa her bir aşamayı ayrı koşu olarak ekleyebilirsiniz.");
-            
+        //Git deneme comment
             Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
         List<Jogging> joggingList = new List<Jogging>();
         Double durationSec = 0;
